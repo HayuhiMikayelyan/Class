dad110a707c09280e0a8b37e301f14d5e7bcc1ed
HayuhiM <hayuhimikayelyan@gmail.com>
2023-02-14 10:28:35 -0800

    Home navigation

--- app/src/main/java/com/example/aclass/HomeFragment.java
+++ app/src/main/java/com/example/aclass/HomeFragment.java
@@ -1,19 +1,56 @@
 package com.example.aclass;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.aclass.databinding.FragmentHomeBinding;
+import com.example.aclass.home.ClassesFragment;
+import com.example.aclass.home.ProfileFragment;
+import com.example.aclass.home.TestsFragment;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class HomeFragment extends Fragment {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        FragmentHomeBinding binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        ClassesFragment classesFragment = new ClassesFragment();
+        TestsFragment testsFragment = new TestsFragment();
+        ProfileFragment profileFragment = new ProfileFragment();
+
+        loadFragment(classesFragment);
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.classesFragment:
+                    loadFragment(classesFragment);
+                    break;
+                case R.id.testsFragment:
+                    loadFragment(testsFragment);
+                    break;
+                case R.id.profileFragment:
+                    loadFragment(profileFragment);
+                    break;
+            }
+            return true;
+        });
+
+        return binding.getRoot();
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getParentFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 }
